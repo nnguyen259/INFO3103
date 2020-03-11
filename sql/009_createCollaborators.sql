63cc73ea867c86a3406a65f46861066c50da9f41
TristanCarrier <tlctristancarrier@gmail.com>
2020-03-11 15:11:07 -0300

    Updated (Please review) sql/009 Created stored procedures

--- sql/009_createCollaborators.sql
+++ sql/009_createCollaborators.sql
@@ -5,3 +5,22 @@ CREATE TABLE collaborators (
 	PRIMARY KEY (username, project_id),
 	FOREIGN KEY (project_id) REFERENCES projects(id)
 );
+DROP PROCEDURE IF EXISTS addCollaborator;
+DROP PROCEDURE IF EXISTS deleteCollaborator;
+
+DELIMITER //
+
+CREATE PROCEDURE addCollaborator(IN usernameIn VARCHAR(50),IN idIn INT)
+begin
+INSERT INTO collaborators (username, project_id) VALUES
+(usernameIn, idIn)
+
+END //
+
+CREATE PROCEDURE deleteCollaborator(IN usernameIn VARCHAR(50), In idIN INT)
+begin
+DELETE FROM collaborator WHERE username = usernameIn AND project_id = idIN;
+
+END //
+
+DELIMITER ;
