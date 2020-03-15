c0903f38622bd35b381801aabd19634ec5d3d1c1
Nhan Nguyen <Nhan.Nguyen@unb.ca>
2020-03-15 16:24:34 -0300

    fix typo

--- sql/013_populateExtraProjects.sql
+++ sql/013_populateExtraProjects.sql
@@ -3,7 +3,7 @@ CALL duplicateProject(0, 1);
 
 CALL createProject('Not Owned Public', 'root', true);
 CALL duplicateProject(0, 2);
-CALL addCollaborator('nnguyen1', 2)
+CALL addCollaborator('nnguyen1', 2);
 
 CALL createProject('Not Owned Private', 'root', false);
 CALL duplicateProject(0, 3);
